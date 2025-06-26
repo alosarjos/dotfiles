dfbb717995cbd93fd697a1e9f86c06e41fa82beb
José Miguel Sarasola <alosarjos@gmail.com>
2025-06-26 09:14:15 +0200

    Move from OhMyPosh to Starship

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -21,11 +21,6 @@ alias ls='eza -g --group-directories-first --git'
 alias lsd='eza -gd --group-directories-first --git'
 alias lsdl='eza -gdl --group-directories-first --git'
 
-function rerender_on_dir_change --on-variable PWD
-    omp_repaint_prompt
-end
-
 if status is-interactive
-    # Load oh-my-posh with custom config
-    oh-my-posh init fish --config ~/.config/oh-my-posh.json | source
+    starship init fish | source
 end
